b55f92146a65a6537c3339861d5009bd707c7876
Jeremy M Rivera <fangman09@gmail.com>
2023-05-28 15:05:07 -0400

    added hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,28 @@
+import Button from './Button';
+import Image from 'next/image';
+import styles from '../styles/Hero.module.scss';
+import Header from './Header';
+
+export default function Hero() {
+  return (
+    <div className={styles.hero}>
+      <Header />
+      <div className={styles.hero__column}>
+        <div className={styles.hero__content}>
+          <h1 className={styles.hero__title}>
+            Simplify Your Promotional Products Experience
+          </h1>
+          <p className={styles.hero__subtitle}>
+            Let us curate the perfect bundle tailored to your industry and needs
+          </p>
+          <Button type='startQuote' />
+        </div>
+        <div className={styles.hero__container}>
+          <div className={styles.hero__image}>
+            <Image src='/Hero.svg' alt='Models' height={761} width={966} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,13 +1,13 @@
-import Header from './Header';
 import Footer from './Footer';
 import HowItWorks from './HowItWorks';
 import FAQPage from './FaqPage';
 import ContactUs from './ContactUs';
+import Hero from './Hero';
 
 export default function HomePage() {
   return (
     <main>
-      <Header />
+      <Hero />
       <HowItWorks />
       <FAQPage />
       <ContactUs />
